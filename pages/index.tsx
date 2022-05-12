4c4b4db41a48d52e080ed9e435db6f21b6c9acc4
Burak Sakalli <buraksakalli06@gmail.com>
2022-05-12 22:05:03 +0400

    feat(setup): jest, husky, commitizen integrations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import Button from "@components/Button";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div className="text-black">
-      <Button>Hello</Button>
+      <h1>Hello</h1>
     </div>
   );
 };
